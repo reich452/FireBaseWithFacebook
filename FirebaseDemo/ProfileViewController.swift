fe4c755b8d4ee0b43fa2f0b67e84a1c83e146bfe
reich452 <reich452@regis.edu>
2018-05-06 17:30:26 -0600

    Updated LogOut Function

--- FirebaseDemo/ProfileViewController.swift
+++ FirebaseDemo/ProfileViewController.swift
@@ -12,29 +12,53 @@ import Firebase
 class ProfileViewController: UIViewController {
     
     @IBOutlet var nameLabel: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.title = "My Profile"
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        updateViews()
     }
     
+    @IBAction func logOutButtonTapped(_ sender: LineButton) {
+        handelLogout()
+    }
     @IBAction func close(sender: AnyObject) {
         dismiss(animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func updateViews() {
+        title = "My Profile"
+        if let currentUser = Auth.auth().currentUser {
+            nameLabel.text = currentUser.displayName
+        }
     }
-    */
-
+    
+    func handelLogout() {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            let alertController = UIAlertController(title: "Logout Error", message: error.localizedDescription, preferredStyle: .alert)
+            let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(okayAction)
+            present(alertController, animated: true, completion: nil)
+            print("Error with sign out \(error) \(error.localizedDescription)")
+            return
+        }
+        if let welcomeVC = self.storyboard?.instantiateViewController(withIdentifier: "WelcomeView") {
+            UIApplication.shared.keyWindow?.rootViewController = welcomeVC
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+    }
 }
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destinationViewController.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+
