7cdb2cb5bc396a78a5e5aa9e70e0b81ccf439f28
reich452 <reich452@regis.edu>
2018-05-06 17:16:00 -0600

    Hanedled loging

--- FirebaseDemo/LoginViewController.swift
+++ FirebaseDemo/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
     
@@ -15,11 +16,6 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,7 +30,42 @@ class LoginViewController: UIViewController {
         
         self.title = ""
     }
+    
 
+    @IBAction func loginButtonTapped(_ sender: LineButton) {
+        handelLogin()
+    }
+    
+    func handelLogin() {
+        guard let emailAddress = emailTextField.text, emailAddress != "",
+        let password = passwordTextField.text, password != "" else {
+            let alertController = UIAlertController(title: "Login Error", message: "Both fields must not be blank.", preferredStyle: .alert)
+            let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(okayAction)
+            present(alertController, animated: true, completion: nil)
+
+            return
+        }
+        
+    
+        Auth.auth().signIn(withEmail: emailAddress, password: password) { (user, error) in
+            if let error = error {
+                print("Error loging in - \(#function): \(error) \(error.localizedDescription)")
+                let alertController = UIAlertController(title: "Login Error", message: error.localizedDescription, preferredStyle: .alert)
+                let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(okayAction)
+                self.present(alertController, animated: true, completion: nil)
+                return
+            }
+            self.view.endEditing(true)
+            
+            if let mainController = self.storyboard?.instantiateViewController(withIdentifier: "MainView") {
+                UIApplication.shared.keyWindow?.rootViewController = mainController
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
+        
+    }
     /*
     // MARK: - Navigation
 
